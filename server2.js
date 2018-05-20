3161c49bce34462104190fa5101f63f6d00dc994
Argir Argirov <dj.giro@gmail.com>
2018-05-20 11:14:44 -0400

    mutation implemented

--- server2.js
+++ server2.js
@@ -6,7 +6,10 @@ const schema = buildSchema(`
     type Query {
         course(id: Int!): Course
         courses(topic: String): [Course]
-    },
+    }
+    type Mutation {
+        updateCourseTopic(id: Int!, topic: String!): Course
+    }
     type Course {
         id: Int
         title: String
@@ -57,9 +60,20 @@ const getCourses = function(args) {
         return coursesData;
     }
 }
+const updateCourseTopic = ({id, topic}) => {
+  coursesData.map(course => {
+    if (course.id === id) {
+      course.topic = topic
+      return course
+    }
+  })
+  return coursesData.filter(course => course.id === id)[0]
+}
+
 const root = {
     course: getCourse,
-    courses: getCourses
+    courses: getCourses,
+    updateCourseTopic: updateCourseTopic
 };
 // Create an express server and a GraphQL endpoint
 const app = express();
